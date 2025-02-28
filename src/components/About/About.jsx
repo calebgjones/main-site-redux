fe30d974467bcffadede5632a9b9b90ebd61da7a
Caleb <apple@Calebs-MacBook-Pro.local>
2025-02-28 08:21:51 -0700

    Balls

---
+++ src/components/About/About.jsx
@@ -0,0 +1,10 @@
+import '../ContentArea/ContentArea.css';
+import { useState, useEffect } from 'react';
+
+function About() {
+    return (
+        <div className='contentArea'>
+            <div id='about' className='contentContainer1'>rararara</div>
+            </div>
+    )};
+export default About;
